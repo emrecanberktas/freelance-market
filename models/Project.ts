7457e5394e72770382edabafae2057cca84fc238
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-05-09 15:19:45 +0300

    Add: Project Model

---
+++ models/Project.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  budget: { type: Number, required: true },
+  deadline: { type: Date, required: true },
+  files: [{ type: String }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.Project ||
+  mongoose.model("Project", ProjectSchema);
