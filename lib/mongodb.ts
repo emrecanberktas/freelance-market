9897cc171c73315ef627e97f5c3fdfc3598202be
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-05-13 14:55:23 +0300

    Chore: client promise

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,14 +1,28 @@
-import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
 
-export const connectMongoDB = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
 
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log("DB Connected");
-  } catch (error) {
-    throw new Error("Failed to connect DB");
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-};
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
